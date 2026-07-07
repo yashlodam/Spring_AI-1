62c80cf8c34facfd88388952e99fbbdf7ef284e6
Yash Lodam <yashlodam03@gmail.com>
2026-07-07 11:09:00 +0530

    storing message in db with the help of jdbcChatMemoryRepository

--- src/main/java/com/ai/config/AiConfig.java
+++ src/main/java/com/ai/config/AiConfig.java
@@ -6,6 +6,8 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,16 @@ import com.ai.advisors.TokenPrintAdvisor;
 
 @Configuration
 public class AiConfig {
+	
+	@Bean
+	public ChatMemory chatMemory(JdbcChatMemoryRepository jdbcchatMemoryRespository) {
+		
+		return MessageWindowChatMemory.builder()
+				.chatMemoryRepository(jdbcchatMemoryRespository)
+				.maxMessages(10)
+				.build();
+	}
+	
 
     private static final Logger logger =
             LoggerFactory.getLogger(AiConfig.class);
@@ -31,8 +43,7 @@ public class AiConfig {
 
         return builder
                 .defaultAdvisors(
-                        memoryAdvisor,
-                        new SimpleLoggerAdvisor()
+                        memoryAdvisor
                 )
                 .defaultSystem("""
                         You are a helpful coding assistant.
